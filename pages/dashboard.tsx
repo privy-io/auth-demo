91bae94d5793790a49632e99ebeede6997d2e550
Nicholas Charriere <nicholascharriere@gmail.com>
2022-12-13 14:13:32 -0800

    Add prettier to package.json and run linter

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -44,7 +44,7 @@ export default function LoginPage() {
   const discordSubject = user?.discord?.subject || null;
 
   if (!ready || !authenticated || !user) {
-    return <Loading />
+    return <Loading />;
   }
   return (
     <>
@@ -53,149 +53,150 @@ export default function LoginPage() {
       </Head>
 
       <main className="flex flex-col min-h-screen px-4 sm:px-20 py-6 sm:py-10 bg-privy-light-blue">
-          <>
-            <div className="flex flex-row justify-between">
-	      <div>
-              <h1 className="text-3xl font-semibold text-gray-800">Privy Auth Demo</h1>
-		<p>You are now authenticated with Privy! You can see which accounts are currently linked to your user on the right.</p>
-		<p>Let&rsquo;s dive into how this works.</p>
-	      </div>
-              <button
-                onClick={logout}
-                className="text-sm bg-violet-200 hover:text-violet-900 py-2 px-4 rounded-md text-violet-700"
-              >
-                Logout
-              </button>
-            </div>
-	    <UserBox user={user} />
-            <div className="mt-12 flex gap-4 flex-wrap">
-              {googleSubject ? (
-                <button
-                  onClick={() => {
-                    unlinkGoogle(googleSubject);
-                  }}
-                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
-                  disabled={!canRemoveAccount}
-                >
-                  Unlink Google
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    linkGoogle();
-                  }}
-                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
-                >
-                  Link Google
-                </button>
-              )}
+        <div className="flex flex-row justify-between">
+          <div>
+            <h1 className="text-3xl font-semibold text-gray-800">Privy Auth Demo</h1>
+            <p>
+              You are now authenticated with Privy! You can see which accounts are currently linked
+              to your user on the right.
+            </p>
+            <p>Let&rsquo;s dive into how this works.</p>
+          </div>
+          <button
+            onClick={logout}
+            className="text-sm bg-violet-200 hover:text-violet-900 py-2 px-4 rounded-md text-violet-700"
+          >
+            Logout
+          </button>
+        </div>
+        <UserBox user={user} />
+        <div className="mt-12 flex gap-4 flex-wrap">
+          {googleSubject ? (
+            <button
+              onClick={() => {
+                unlinkGoogle(googleSubject);
+              }}
+              className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+              disabled={!canRemoveAccount}
+            >
+              Unlink Google
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                linkGoogle();
+              }}
+              className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
+            >
+              Link Google
+            </button>
+          )}
 
-              {twitterSubject ? (
-                <button
-                  onClick={() => {
-                    unlinkTwitter(twitterSubject);
-                  }}
-                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
-                  disabled={!canRemoveAccount}
-                >
-                  Unlink Twitter
-                </button>
-              ) : (
-                <button
-                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
-                  onClick={() => {
-                    linkTwitter();
-                  }}
-                >
-                  Link Twitter
-                </button>
-              )}
+          {twitterSubject ? (
+            <button
+              onClick={() => {
+                unlinkTwitter(twitterSubject);
+              }}
+              className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+              disabled={!canRemoveAccount}
+            >
+              Unlink Twitter
+            </button>
+          ) : (
+            <button
+              className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
+              onClick={() => {
+                linkTwitter();
+              }}
+            >
+              Link Twitter
+            </button>
+          )}
 
-              {discordSubject ? (
-                <button
-                  onClick={() => {
-                    unlinkDiscord(discordSubject);
-                  }}
-                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
-                  disabled={!canRemoveAccount}
-                >
-                  Unlink Discord
-                </button>
-              ) : (
-                <button
-                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
-                  onClick={() => {
-                    linkDiscord();
-                  }}
-                >
-                  Link Discord
-                </button>
-              )}
+          {discordSubject ? (
+            <button
+              onClick={() => {
+                unlinkDiscord(discordSubject);
+              }}
+              className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+              disabled={!canRemoveAccount}
+            >
+              Unlink Discord
+            </button>
+          ) : (
+            <button
+              className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
+              onClick={() => {
+                linkDiscord();
+              }}
+            >
+              Link Discord
+            </button>
+          )}
 
-              {email ? (
-                <button
-                  onClick={() => {
-                    unlinkEmail(email.address);
-                  }}
-                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
-                  disabled={!canRemoveAccount}
-                >
-                  Unlink email
-                </button>
-              ) : (
-                <button
-                  onClick={linkEmail}
-                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
-                >
-                  Connect email
-                </button>
-              )}
-              {wallet ? (
-                <button
-                  onClick={() => {
-                    unlinkWallet(wallet.address);
-                  }}
-                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
-                  disabled={!canRemoveAccount}
-                >
-                  Unlink wallet
-                </button>
-              ) : (
-                <button
-                  onClick={linkWallet}
-                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white border-none"
-                >
-                  Connect wallet
-                </button>
-              )}
-              {phone ? (
-                <button
-                  onClick={() => {
-                    unlinkPhone(phone.number);
-                  }}
-                  className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
-                  disabled={!canRemoveAccount}
-                >
-                  Unlink phone
-                </button>
-              ) : (
-                <button
-                  onClick={linkPhone}
-                  className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white border-none"
-                >
-                  Connect phone
-                </button>
-              )}
-            </div>
+          {email ? (
+            <button
+              onClick={() => {
+                unlinkEmail(email.address);
+              }}
+              className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+              disabled={!canRemoveAccount}
+            >
+              Unlink email
+            </button>
+          ) : (
+            <button
+              onClick={linkEmail}
+              className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
+            >
+              Connect email
+            </button>
+          )}
+          {wallet ? (
+            <button
+              onClick={() => {
+                unlinkWallet(wallet.address);
+              }}
+              className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+              disabled={!canRemoveAccount}
+            >
+              Unlink wallet
+            </button>
+          ) : (
+            <button
+              onClick={linkWallet}
+              className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white border-none"
+            >
+              Connect wallet
+            </button>
+          )}
+          {phone ? (
+            <button
+              onClick={() => {
+                unlinkPhone(phone.number);
+              }}
+              className="text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+              disabled={!canRemoveAccount}
+            >
+              Unlink phone
+            </button>
+          ) : (
+            <button
+              onClick={linkPhone}
+              className="text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white border-none"
+            >
+              Connect phone
+            </button>
+          )}
+        </div>
 
-            <p className="mt-6 font-bold uppercase text-sm text-gray-600">User object</p>
-            <textarea
-              value={JSON.stringify(user, null, 2)}
-              className="max-w-4xl bg-slate-700 text-slate-50 font-mono p-4 text-xs sm:text-sm rounded-md mt-2"
-              rows={20}
-              disabled
-            />
-          </>
+        <p className="mt-6 font-bold uppercase text-sm text-gray-600">User object</p>
+        <textarea
+          value={JSON.stringify(user, null, 2)}
+          className="max-w-4xl bg-slate-700 text-slate-50 font-mono p-4 text-xs sm:text-sm rounded-md mt-2"
+          rows={20}
+          disabled
+        />
       </main>
     </>
   );
