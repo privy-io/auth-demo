09c9a8a8d5a0d3b7618bbf1f0c6de11fa6569afd
Nicholas Charriere <nicholascharriere@gmail.com>
2022-12-13 21:14:29 -0800

    Fix linter

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,20 +13,23 @@ export default function LoginPage() {
 
       <main className="flex min-h-screen min-w-full bg-privy-light-blue justify-center">
         <div className="flex flex-col flex-1 p-6 justify-center items-center max-w-3xl">
-	    <h1 className="text-5xl font-bold text-gray-800">Privy Auth</h1>
-	    <p className="text-xl text-gray-700 px-10 pt-4">Privy Auth is a developer library that allows you to easily integrate progressive, user centric authentication into your web application.</p>
-	    <p className="text-xl text-gray-700 px-10 pt-4">Try it out!</p>
-            <div>
-              <Portal style={{maxWidth: '100%', height: 'auto'}} />
-            </div>
-            <div className="mt-6 flex justify-center text-center mb-10">
-              <button
-                className="bg-violet-600 hover:bg-violet-700 py-3 px-6 text-xl text-white rounded-lg"
-                onClick={login}
-              >
-                Log in
-              </button>
-            </div>
+          <h1 className="text-5xl font-bold text-gray-800">Privy Auth</h1>
+          <p className="text-xl text-gray-700 px-10 pt-4">
+            Privy Auth is a developer library that allows you to easily integrate progressive, user
+            centric authentication into your web application.
+          </p>
+          <p className="text-xl text-gray-700 px-10 pt-4">Try it out!</p>
+          <div>
+            <Portal style={{maxWidth: '100%', height: 'auto'}} />
+          </div>
+          <div className="mt-6 flex justify-center text-center mb-10">
+            <button
+              className="bg-violet-600 hover:bg-violet-700 py-3 px-6 text-xl text-white rounded-lg"
+              onClick={login}
+            >
+              Log in
+            </button>
+          </div>
         </div>
       </main>
     </>
