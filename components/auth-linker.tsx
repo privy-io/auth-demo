3b6ec3228be27dd615c3f1cf7757ab383478909b
Nicholas Charriere <nicholascharriere@gmail.com>
2022-12-13 21:35:28 -0800

    Fix colors, fuck violet

--- components/auth-linker.tsx
+++ components/auth-linker.tsx
@@ -27,7 +27,7 @@ export default function AuthLinker({
           onClick={() => {
             unlinkAction();
           }}
-          className="min-w-[250px] text-sm border border-violet-600 hover:border-violet-700 py-2 px-4 rounded-md text-violet-600 hover:text-violet-700 disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
+          className="min-w-[250px] text-sm border border-slate-700 hover:border-black py-2 px-4 rounded-md text-slate-700 hover:text-black disabled:border-gray-500 disabled:text-gray-500 hover:disabled:text-gray-500"
           disabled={!canRemove}
         >
           {unlinkCta}
@@ -37,7 +37,7 @@ export default function AuthLinker({
           onClick={() => {
             linkAction();
           }}
-          className="min-w-[250px] text-sm bg-violet-600 hover:bg-violet-700 py-2 px-4 rounded-md text-white"
+          className="min-w-[250px] text-sm bg-slate-800 hover:bg-slate-900 py-2 px-4 rounded-md text-white"
         >
           {linkCta}
         </button>
