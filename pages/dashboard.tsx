e0d3f8f0ac74c11351cdb08326fe2a629cb37d57
Nicholas Charriere <nicholascharriere@gmail.com>
2022-12-14 17:02:54 -0800

    Fix padding (#30)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -103,7 +103,7 @@ export default function LoginPage() {
           <UserBox user={user} />
         </div>
 
-        <div id="columns" className="grid grid-cols-3 mt-24 gap-10 -sm:grid-cols-1">
+        <div id="columns" className="grid grid-cols-3 mt-24 gap-10 -sm:grid-cols-1 -sm:mt-6">
           <div>
             <h2 className="font-bold uppercase text-lg text-slate-700">Engage your users</h2>
             <p className="text-sm text-slate-600 min-h-[60px]">
