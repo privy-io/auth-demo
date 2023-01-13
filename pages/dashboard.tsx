dff1f90ef199bbbc7e613433739b51b3aee4ed0d
Nicholas Charriere <nicholascharriere@gmail.com>
2023-01-13 09:27:40 -0800

    Fix length of textarea

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -229,11 +229,11 @@ export default function LoginPage() {
               .
             </p>
             <h3 className="font-bold text-privy-navy text-lg mt-5 lg:mt-0">JSON</h3>
-            <div className="mt-5">
+            <div className="mt-5 min-h-full">
               <textarea
                 value={JSON.stringify(user, null, 2)}
                 className="min-w-full min-h-full p-5 bg-white text-privy-navy font-mono text-xs rounded-xl border-0"
-                rows={25}
+                rows={JSON.stringify(user, null, 2).split('\n').length}
                 disabled
               />
             </div>
