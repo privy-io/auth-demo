cfdf2d9c42006068f896cc05f88cf1adf47d958c
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2023-01-14 11:12:46 -0500

    Fix format

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -240,7 +240,9 @@ export default function LoginPage() {
           </div>
 
           <div className="hidden lg:block">
-            <h2 className="font-bold text-2xl text-privy-navy text-xl md:text-2xl">Work with responsive UI</h2>
+            <h2 className="font-bold text-2xl text-privy-navy text-xl md:text-2xl">
+              Work with responsive UI
+            </h2>
             <p className="text-sm min-h-[60px] mt-4">
               You decide when to engage users, we take care of the how. Connect within seconds,
               seriously.
