1e6278c12f13c5eefc1d0a62ed26622dd8c72acd
Henri S <sternhenri@users.noreply.github.com>
2023-02-02 23:38:43 -0500

    fix logo skew

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -96,7 +96,7 @@ export default function LoginPage() {
         <div className="hidden sm:block">
           <div className="flex flex-row justify-between items-center">
             <div>
-              <Image src="logos/privy_demo.png" height="50px" width="100%" alt="Privy Auth Demo" />
+              <Image src="/logos/privy-demo.png" height="50px" width="206px" alt="Privy Auth Demo" />
             </div>
             <div className="flex gap-4 items-center justify-center">
               <p className="underline hover:cursor-pointer text-privurple hover:text-privurpleaccent">
