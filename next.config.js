45d13aa01ccf4dfcaebc72d5b44617144180bb8e
Benjamin <bredfield@gmail.com>
2023-06-19 19:05:01 -0400

    bump sdk, remove docs link on mobile for clarity (#111)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const {withPlausibleProxy} = require('next-plausible');
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 module.exports = withPlausibleProxy()({
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // This is hammer approach for local dev. We can't figure out why, but node 18 starts to have
+      // issues with local dev seeing multiple reacts.
+      react: path.resolve('./node_modules/react'),
+    };
+    return config;
+  },
 });
