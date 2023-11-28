667ef2f0b457ffd4e2cf6a2fbad895a6b2f0c8df
Joshua Navi <josh@privy.io>
2023-11-27 17:45:23 -0800

    fix(demo): set max-width same as min-width, 360px (#163)

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,7 +125,10 @@ export default function LoginPage() {
           {/* start: canvas-panel */}
           <Canvas className="md:pl-20 md:pr-8">
             {/* start: modal-column */}
-            <div id="render-privy" className="z-[2] mx-auto min-w-[360px] pt-8 md:mx-0 md:pt-0" />
+            <div
+              id="render-privy"
+              className="z-[2] mx-auto min-w-[360px] max-w-[360px] pt-8 md:mx-0 md:pt-0"
+            />
             {/* end: modal-column */}
             <CanvasRow className="hidden md:flex">
               {/* start: cta */}
