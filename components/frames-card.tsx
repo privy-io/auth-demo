33182136dd307abb50f34084d8f8a8c9fc2e40a8
Ankush Swarnakar <ankushswar1@gmail.com>
2024-02-01 17:24:31 -0500

    Only show frame for NFT holders

--- components/frames-card.tsx
+++ components/frames-card.tsx
@@ -55,6 +55,8 @@ export default function FramesCard() {
     getFramesNftOwnership(embeddedWallet.address);
   }, [farcasterAccount, embeddedWallet]);
 
+  if (!farcasterAccount || !embeddedWallet || !hasFramesNft) return null;
+
   return (
     <Transition
       appear={true}
